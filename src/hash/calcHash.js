ed2b79d8697c02ff4ac189aace274b2db3841e89
Evgeniy Dolgov <dolgov.e.1991@gmail.com>
2024-10-07 05:55:42 +0300

    feat: add hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+import crypto from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToRead = path.join(
+    __dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  try {
+    const data = await fs.promises.readFile(fileToRead);
+    const hash = crypto.createHash("sha256");
+    hash.update(data);
+
+    const hexHash = hash.digest("hex");
+    console.log(hexHash);
+  } catch (error) {
+    throw new Error(err);
+  }
 };
 
 await calculateHash();
